b3751efab23e3b9d44378305c4fc8c1c197ad0fa
Mayur Kulkarni <mayurkulkarni012@gmail.com>
2018-01-25 10:35:04 +0530

    Started Silhoutte Distance

---
+++ 07. Reinforcement Learning/random_selection.py
@@ -0,0 +1,28 @@
+# Random Selection
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Ads_CTR_Optimisation.csv')
+
+# Implementing Random Selection
+import random
+N = 10000
+d = 10
+ads_selected = []
+total_reward = 0
+for n in range(0, N):
+    ad = random.randrange(d)
+    ads_selected.append(ad)
+    reward = dataset.values[n, ad]
+    total_reward = total_reward + reward
+
+# Visualising the results
+plt.hist(ads_selected)
+plt.title('Histogram of ads selections')
+plt.xlabel('Ads')
+plt.ylabel('Number of times each ad was selected')
+plt.show()
